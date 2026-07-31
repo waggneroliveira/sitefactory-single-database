884ad9c93da4a409a44000dfe43c5b08562a6f3c
Front-end-4 <waggner.dev@gmail.com>
2026-07-31 10:34:19 -0300

    Update rota com middleware([NeedsTenant::class])
    adicionando as rota no middleware([NeedsTenant::class])

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,11 @@ use App\Http\Controllers\Auth\AuthClientController;
 use App\Http\Controllers\Auth\PasswordEmailClientController;
 use App\Http\Controllers\Auth\ResetPasswordClientController;
 use App\Http\Controllers\Client\AboutPageController;
-use App\Http\Controllers\Client\BenefitPageController;
 use App\Http\Controllers\Client\BlogPageController;
 use App\Http\Controllers\Client\ContactPageController;
 use App\Http\Controllers\Client\EventPageController;
 use App\Http\Controllers\Client\HomePageController;
-use App\Http\Controllers\Client\JuridicoPageController;
-use App\Http\Controllers\Client\NoticiesPageController;
 use App\Http\Controllers\Client\ProductPageController;
-use App\Http\Controllers\Client\RegionPageController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DownloadFichaController;
@@ -20,112 +16,194 @@ use App\Http\Controllers\FormIndexController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Middleware\AuthClientMiddleware;
 use App\Models\About;
-use App\Models\Agreement;
 use App\Models\Announcement;
 use App\Models\BenefitTopic;
 use App\Models\BlogCategory;
 use App\Models\Contact;
 use App\Models\Direction;
 use App\Models\Report;
-use App\Models\Statute;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
-use Inertia\Inertia;
+use Spatie\Multitenancy\Http\Middleware\NeedsTenant;
 
 require __DIR__ . '/dashboard.php';
 
-Route::get('/', function () {
-    return redirect()->route('index');
+
+// =============================
+// Rotas que NÃO precisam tenant
+// =============================
+
+
+// =============================
+// Rotas dos clientes/tenants
+// =============================
+
+Route::middleware([NeedsTenant::class])->group(function () {
+
+
+    Route::get('/', [HomePageController::class, 'index'])
+        ->name('index');
+
+
+    Route::get('produto/{category}/{slug}', [ProductPageController::class, 'productView'])
+        ->name('client.product');
+
+
+    Route::get('produtos', [ProductPageController::class, 'productAll'])
+        ->name('products');
+
+
+    Route::post('login.do', [AuthClientController::class, 'authenticate'])
+        ->name('client.user.authenticate');
+
+
+    Route::post('/password/email', [PasswordEmailClientController::class, 'passwordEmail'])
+        ->name('client.password.email');
+
+
+    Route::get('/email-enviado-com-sucesso', [PasswordEmailClientController::class, 'showSuccess'])
+        ->name('send-success-client');
+
+
+    Route::post('/password/reset', [ResetPasswordClientController::class, 'processPasswordReset'])
+        ->name('client-password.update');
+
+
+    Route::get('password/reset/{token}', [ResetPasswordClientController::class, 'showResetForm'])
+        ->name('client.password.reset');
+
+
+    Route::get('/senha-alterada-com-sucesso', function () {
+        return view('emails.password-success-client-reset');
+    })->name('client-success-reset-password');
+
+
+    Route::middleware([AuthClientMiddleware::class])->group(function () {
+
+        Route::put('/client/update', [ClientController::class, 'update'])
+            ->name('client.update');
+
+
+        Route::post('/client/comments', [CommentController::class, 'store'])
+            ->name('blog.comment');
+
+
+        Route::get('logout', [AuthClientController::class, 'logout'])
+            ->name('client.user.logout');
+
+    });
+
+
+    Route::get('imovel', function () {
+        return view('client.themes.corretora.tp-01.blades.imovel');
+    })->name('imovel');
+
+
+    Route::get('contato', [ContactPageController::class, 'index'])
+        ->name('contact');
+
+
+    Route::post('send-contact', [FormIndexController::class, 'store'])
+        ->name('send-contact');
+
+
+    Route::get('blog/{slug}', [BlogPageController::class, 'blogInner'])
+        ->name('blog-inner');
+
+
+    Route::get('blog', [BlogPageController::class, 'index'])
+        ->name('blogAll');
+
+
+    Route::get('blog/categoria/{category?}', [BlogPageController::class, 'index'])
+        ->name('blog');
+
+
+    Route::post('blog/search', [BlogPageController::class, 'index'])
+        ->name('blog-search');
+
+
+    Route::post('send-newsletter', [NewsletterController::class, 'store'])
+        ->name('send-newsletter');
+
+
+    Route::post('cliente/cadastro', [ClientController::class, 'store'])
+        ->name('register-client');
+
+
+    Route::get('sobre', [AboutPageController::class, 'index'])
+        ->name('about');
+
+
+    Route::get('eventos', [EventPageController::class, 'index'])
+        ->name('client.event');
+
+
+    Route::get('blog/filter/{category?}', [HomePageController::class, 'filterByCategory'])
+        ->name('blog.filter');
+
+
+    Route::post('/download-ficha/store', [DownloadFichaController::class, 'store'])
+        ->name('download.ficha.store');
+
 });
 
-Route::get('produto/{category}/{slug}', [ProductPageController::class, 'productView'])->name('client.product');
-Route::get('produtos', [ProductPageController::class, 'productAll'])->name('products');
 
-Route::post('login.do', [AuthClientController::class, 'authenticate'])
-->name('client.user.authenticate');
-
-// Rota para processar o formulário "Esqueci a senha"
-Route::post('/password/email', [PasswordEmailClientController::class, 'passwordEmail'])
-->name('client.password.email');
-
-Route::get('/email-enviado-com-sucesso', [PasswordEmailClientController::class, 'showSuccess'])
-->name('send-success-client');
-
-// Rota para processar a redefinição de senha
-Route::post('/password/reset', [ResetPasswordClientController::class, 'processPasswordReset'])
-->name('client-password.update');
-
-// Rota para exibir o formulário de redefinição de senha
-Route::get('password/reset/{token}', [ResetPasswordClientController::class, 'showResetForm'])
-->name('client.password.reset');
-
-
-Route::get('/senha-alterada-com-sucesso', function () {
-    return view('emails.password-success-client-reset');
-})->name('client-success-reset-password');
-
-
-Route::middleware([AuthClientMiddleware::class])->group(function () {
-    Route::put('/client/update', [ClientController::class, 'update'])->name('client.update');
-
-    Route::post('/client/comments', [CommentController::class, 'store'])
-    ->name('blog.comment');
-
-    Route::get('logout', [AuthClientController::class, 'logout'])->name('client.user.logout');
-});
-
-Route::get('/imovel', function () {
-    return view('client.themes.corretora.tp-01.blades.imovel');
-})->name('imovel');
-
-Route::get('contato', [ContactPageController::class, 'index'])
-->name('contact');
-Route::post('send-contact', [FormIndexController::class, 'store'])->name('send-contact');
-Route::get('blog/{slug}', [BlogPageController::class, 'blogInner'])
-->name('blog-inner');
-Route::get('blog', [BlogPageController::class, 'index'])->name('blogAll');
-Route::get('blog/categoria/{category?}', [BlogPageController::class, 'index'])->name('blog');
-Route::post('blog/search', [BlogPageController::class, 'index'])->name('blog-search');
-Route::post('send-newsletter', [NewsletterController::class, 'store'])->name('send-newsletter');
-Route::post('cliente/cadastro', [ClientController::class, 'store'])->name('register-client');
-Route::get('/', [HomePageController::class, 'index'])->name('index');
-Route::get('sobre', [AboutPageController::class, 'index'])->name('about');
-Route::get('eventos', [EventPageController::class, 'index'])->name('client.event');
-Route::get('blog/filter/{category?}', [HomePageController::class, 'filterByCategory'])
-    ->name('blog.filter');
-Route::post('/download-ficha/store', [DownloadFichaController::class, 'store'])
-->name('download.ficha.store');
+// =============================
+// View Composer
+// =============================
 
 View::composer('client.core.client', function ($view) {
+
     $blogCategories = BlogCategory::whereHas('blogs')
-    ->active()
-    ->sorting()
-    ->limit(10)
-    ->get();
+        ->active()
+        ->sorting()
+        ->limit(10)
+        ->get();
+
+
     $announcements = Announcement::select(
         'exhibition',
         'link',
-        'exhibition',
         'path_image',
         'active',
         'sorting',
     )
-    ->where('exhibition', '=', 'mobile')
-    ->orWhere('exhibition', '=', 'horizontal')
+    ->whereIn('exhibition', ['mobile', 'horizontal'])
     ->active()
     ->sorting()
     ->get();
-    $contact = Contact::first();
-    $abouts = About::active()->sorting()->get();
-    $directions = Direction::active()->sorting()->count();
-    $benefitTopics = BenefitTopic::active()->sorting()->count();
-    $report = Report::active()->count();
 
-    return $view->with('blogCategories', $blogCategories)
-    ->with('announcements', $announcements)
-    ->with('contact', $contact)
-    ->with('directions', $directions)
-    ->with('benefitTopics', $benefitTopics)
-    ->with('report', $report)
-    ->with('abouts', $abouts);
+
+    $contact = Contact::first();
+
+    $abouts = About::active()
+        ->sorting()
+        ->get();
+
+
+    $directions = Direction::active()
+        ->sorting()
+        ->count();
+
+
+    $benefitTopics = BenefitTopic::active()
+        ->sorting()
+        ->count();
+
+
+    $report = Report::active()
+        ->count();
+
+
+    return $view->with([
+        'blogCategories' => $blogCategories,
+        'announcements' => $announcements,
+        'contact' => $contact,
+        'directions' => $directions,
+        'benefitTopics' => $benefitTopics,
+        'report' => $report,
+        'abouts' => $abouts,
+    ]);
+
 });
