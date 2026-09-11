cc006c150ab9b0759962a326192729d9a6828292
Wagner Oliveira <waggner.dev@gmail.com>
2026-09-11 11:27:04 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
             if (!view()->exists($view)) {
                 return response()->view(
-                    'client.errors.404',
+                    'client.tenant-error.404',
                     [],
                     404
                 );
