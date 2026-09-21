1b93cb3e864b5edf76a697967039cda74ff4d988
Wagner Oliveira <waggner.dev@gmail.com>
2026-09-21 19:38:58 -0300

    Update form.blade.php

--- resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
+++ resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
@@ -50,29 +50,29 @@
 </div>
 
 {{-- IMAGEM --}}
-@if (isset($advantage) && $advantage->path_image != null && $forYou === 'enterprise')
-    <div class="row" id="image-field{{ isset($advantage->id) ? $advantage->id : '' }}">
-        <div class="col-12">
-            <div class="mt-3">
-                <label for="path_image" class="form-label">
-                    Imagem
-                </label>
+{{-- @if (isset($advantage) && $advantage->path_image != null && $forYou === 'enterprise')
+@endif --}}
+<div class="row" id="image-field{{ isset($advantage->id) ? $advantage->id : '' }}">
+    <div class="col-12">
+        <div class="mt-3">
+            <label for="path_image" class="form-label">
+                Imagem
+            </label>
 
-                <input
-                    type="file"
-                    name="path_image"
-                    data-plugins="dropify"
-                    data-default-file="{{ isset($advantage) && $advantage->path_image != '' ? url('storage/' . $advantage->path_image) : '' }}"
-                />
+            <input
+                type="file"
+                name="path_image"
+                data-plugins="dropify"
+                data-default-file="{{ isset($advantage) && $advantage->path_image != '' ? url('storage/' . $advantage->path_image) : '' }}"
+            />
 
-                <p class="text-muted text-center mt-2 mb-0">
-                    {{ __('dashboard.text_img_size') }}
-                    <b class="text-danger">2 MB</b>.
-                </p>
-            </div>
+            <p class="text-muted text-center mt-2 mb-0">
+                {{ __('dashboard.text_img_size') }}
+                <b class="text-danger">2 MB</b>.
+            </p>
         </div>
     </div>
-@endif
+</div>
 
 <div class="mb-3">
     <div class="form-check">
