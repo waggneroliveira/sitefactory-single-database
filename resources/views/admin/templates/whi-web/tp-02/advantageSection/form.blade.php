68f2c73b5a7f50e4768e65b3ff5683e4e1981742
Wagner Oliveira <waggner.dev@gmail.com>
2026-09-21 19:35:10 -0300

    Update form.blade.php

--- resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
+++ resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
@@ -50,6 +50,7 @@
 </div>
 
 {{-- IMAGEM --}}
+{{dd($forYou)}}
 @if ($forYou === 'enterprise')
     <div class="row" id="image-field{{ isset($advantage->id) ? $advantage->id : '' }}">
         <div class="col-12">
