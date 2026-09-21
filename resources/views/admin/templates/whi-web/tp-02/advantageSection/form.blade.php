e96b7833837a54a10d0e63c37fb0a3c893ca01d3
Wagner Oliveira <waggner.dev@gmail.com>
2026-09-21 19:50:08 -0300

    Update form.blade.php

--- resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
+++ resources/views/admin/templates/whi-web/tp-02/advantageSection/form.blade.php
@@ -64,8 +64,8 @@
 </div>
 
 {{-- IMAGEM --}}
-@if (trim(strtolower($forYou)) === 'enterprise')
-    <div class="row" id="image-field{{ isset($advantage->id) ? $advantage->id : '' }}">
+@if (isset($forYou) && strtolower(trim($forYou)) === 'enterprise')
+    <div class="row" id="image-field{{ $advantage->id ?? '' }}">
         <div class="col-12">
             <div class="mt-3">
                 <label for="path_image" class="form-label">
@@ -76,7 +76,7 @@
                     type="file"
                     name="path_image"
                     data-plugins="dropify"
-                    data-default-file="{{ isset($advantage) && $advantage->path_image != '' ? url('storage/' . $advantage->path_image) : '' }}"
+                    data-default-file="{{ !empty($advantage?->path_image) ? url('storage/' . $advantage->path_image) : '' }}"
                 />
 
                 <p class="text-muted text-center mt-2 mb-0">
